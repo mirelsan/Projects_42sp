44ce2d8b70372f4cc29144a70b32e31ab09af1f7
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 <mirelsan42student@gmail.com>
2026-05-23 15:59:36 -0300

    add prototypes

--- 01_Libft_42sp/ft_memmove.c
+++ 01_Libft_42sp/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/05/23 02:06:54 by username         #+#    #+#              */
-/*   Updated: 2026/05/23 14:43:25 by username        ###   ########.fr        */
+/*   Updated: 2026/05/23 15:50:42 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,12 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	size_t	i;
+
+	i = 0;
+	if (src > dest && src + n >= dest)
+	{
+		while (i < n)
+			i++;
+	}
 }
--- 01_Libft_42sp/libft.h
+++ 01_Libft_42sp/libft.h
@@ -6,7 +6,7 @@
 /*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/05/20 15:25:38 by username         #+#    #+#              */
-/*   Updated: 2026/05/23 14:43:49 by username        ###   ########.fr        */
+/*   Updated: 2026/05/23 15:58:45 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 //Libc functions
 int		ft_isalpha(int c);
@@ -54,4 +60,15 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+//Linked List
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void * (*f)(void *), void (*del)(void *));
 #endif

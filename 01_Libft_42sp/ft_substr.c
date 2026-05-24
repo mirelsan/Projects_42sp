19a79a04f1f6b559236d787eeee7e1bce3df0978
mirelsan <mirelsan42student@gmail.com>
2026-05-24 13:18:57 -0500

    update substr and create strjoin

--- 01_Libft_42sp/ft_substr.c
+++ 01_Libft_42sp/ft_substr.c
@@ -6,7 +6,38 @@
 /*   By: mirelapitt <mirelapitt@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/24 12:08:23 by mirelapitt        #+#    #+#             */
-/*   Updated: 2026/05/24 12:11:02 by mirelapitt       ###   ########.fr       */
+/*   Updated: 2026/05/24 13:15:02 by mirelapitt       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+
+    char *substr;
+    size_t s_len;
+       
+        if(!s)
+            return(NULL);
+    
+    s_len = ft_strlen(s);
+    
+    if(s_len < start)
+    {
+        substr = ((char*)malloc(sizeof(char)* 1));
+        if(!substr)
+            return(NULL);
+        substr[0] = '\0';   
+        return(substr);
+    }
+        if(len > s_len - start)
+         len = s_len - start;
+         
+    substr = malloc(len + 1);
+    
+        ft_memcpy(substr, s + start, len);
+        substr[len] = '\0';
+        return(substr);
+}
 

7d67043e460e4baf64a311f55f8eb59ec38cdf17
Mirela Santos Oliveira <mirelsan42student@gmail.com>
2026-05-24 11:24:15 -0300

    update ft_strdup.c

--- 01_Libft_42sp/ft_strdup.c
+++ 01_Libft_42sp/ft_strdup.c
@@ -1,1 +1,21 @@
-char        *ft_strdup(const char *s);
+char        *ft_strdup(const char *s)
+{
+     char *dest;
+     int i;
+     int s_len;
+     
+     
+     s_len = ft_strlen(s);
+     dest = malloc(s_len + 1);
+     
+     if(dest == NULL)
+         return (NULL);
+     i = 0;
+     while(i < s_len)
+     {
+         dest[i] = s[i];
+         i++;
+     }
+     dest[i] = '\0';
+     return(dest);
+}

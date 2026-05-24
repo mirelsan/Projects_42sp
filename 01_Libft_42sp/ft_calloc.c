e25eebace6b740128dca841f5727692c6db54b36
Mirela Santos Oliveira <mirelsan42student@gmail.com>
2026-05-24 09:52:22 -0300

    Actualize ft_calloc.c

--- 01_Libft_42sp/ft_calloc.c
+++ 01_Libft_42sp/ft_calloc.c
@@ -1,1 +1,14 @@
-void        *ft_calloc(size_t nmemb, size_t size);
+#include "libft.h"
+
+void        *ft_calloc(size_t nmemb, size_t size)
+{
+    size_t sum;
+    void *p;
+    
+    sum = (nmemb * size);
+    p = malloc(sum);
+    if(!p)
+        return(NULL);
+    ft_memset(p, 0, sum):
+        return(p);    
+}

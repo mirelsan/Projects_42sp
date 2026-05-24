534c93d39d671f726decd629d097a24f36c4f7f4
Mirela Santos Oliveira <mirelsan42student@gmail.com>
2026-05-24 20:52:59 -0300

    update strjoin

--- 01_Libft_42sp/ft_strjoin.c
+++ 01_Libft_42sp/ft_strjoin.c
@@ -10,7 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+    size_t len_s1;
+    size_t len_s2;
+    char *str_result;
+
+    if(!s1 || !s2)
+        return(NULL);
+
+    len_s1 = ft_strlen(s1);
+    len_s2 = ft_strlen(s2);
+
+    str_result = malloc(len_s1 + len_s2 + 1);
+
+    if(!str_result)
+        return(NULL);
     
+        ft_memcpy(str_result, s1, len_s1);
+        ft_memcpy(str_result + len_s1, s2, len_s2);
+
+        str_result[len_s1 + len_s2] = '\0';
+        return(str_result);
 }

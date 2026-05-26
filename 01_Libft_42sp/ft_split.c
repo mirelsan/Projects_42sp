a9fe33d2c9fb4b7fc9f1bd6038f671de5a9c4ed9
Mirela Santos Oliveira <mirelsan42student@gmail.com>
2026-05-26 20:26:33 -0300

    update ft_split.c

--- 01_Libft_42sp/ft_split.c
+++ 01_Libft_42sp/ft_split.c
@@ -12,25 +12,68 @@
 
 #include "libft.h"
 
-static int	ft_c_split(char const *s, char c)
+static int        ft_string_count(char const *s, char c)
 {
-	int	i;
-	int	words;
+        int        i;
+        int        words;
 
-	i = 0;
-	words = 0;
-	while (s[i])
-	{
-		if (s[i] != 'c')
-		{
-			words++;
-			while (s[i] && s[i] != 'c')
-				i++ :
-		}
-		else
-			i++;
-	}
-	return (words);
+        i = 0;
+        words = 0;
+        while (s[i])
+        {
+                if (s[i] != 'c')
+                {
+                        words++;
+                        while (s[i] && s[i] != 'c')
+                                i++;
+                }
+                else
+                        i++;
+        }
+        return (words);
 }
 
-static int	ft_splitter(void)
+static char        *ft_splitter(char const *s, char c)
+{
+        int i;
+        i = 0;
+        
+        char *word;
+        
+        while(s[i])
+        {
+            if(s[i] != 'c')
+            i++;
+        }
+        
+        word = malloc((char)sizeof(char *)(i + 1));
+        
+        if(!word)
+        refurn (NULL);
+        
+        i = 0;
+        
+        while(s[i] && s[i] != 'c')
+        {
+            word[i] = s[i];
+            i++;
+        }
+        word = '\0'
+        return(word);
+}
+
+static char **ft_split(char const *s, char c)
+{
+    int i;
+    int j;
+    char **words;
+    
+    i = 0;
+    j = 0;
+    
+    words = ((char **)sizeof(char *) * ft_string_count(s,c + 1));
+    
+    if(!words)
+        return(NULL);
+}
+    

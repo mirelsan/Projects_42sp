793072c32d98437ebd59b40d63c568255c0778e1
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 <mirelsan42student@gmail.com>
2026-06-01 13:31:04 -0300

    change linked list names

--- 01_Libft_42sp/ft_memcpy.c
+++ 01_Libft_42sp/ft_memcpy.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mirelapitt <mirelapitt@student.42.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/23 01:31:15 by username          #+#    #+#             */
-/*   Updated: 2026/05/30 18:38:24 by mirelapitt       ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   ft_memcpy.c                                       :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/05/23 01:31:15 by username         #+#    #+#              */
+/*   Updated: 2026/06/01 13:13:49 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (dest == NULL || src == NULL)
+	if (dest == NULL && src == NULL)
 		return (NULL);
 	while (i < n)
 	{

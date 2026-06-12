d0c8a59f4dc0b6eea79b28a17f24d5298d2edafa
Mirela Santos Oliveira <mirelsan42student@gmail.com>
2026-06-12 06:13:24 +0000

    fix norminette

--- 03_get_next_line/get_next_line.h
+++ 03_get_next_line/get_next_line.h
@@ -26,10 +26,10 @@ int		ft_verify_stash(char *stash);
 char	*ft_append_stash(char *stash, int fd);
 char	*ft_fetch_line(char **stash);
 
-char    *ft_strjoin(char const *s1, char const *s2);
-char    *ft_substr(char const *s, unsigned int start, size_t len);
-char    *ft_strchr(const char *s, int c);
-size_t    ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_read_stash(char *stash, int fd);
 

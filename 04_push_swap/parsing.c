58c484a5ab9d502f5828bc9eebc2b5800a763f05
mirelsan <mirelsan42student@gmail.com>
2026-06-15 15:49:58 -0300

    add

--- 04_push_swap/parsing.c
+++ 04_push_swap/parsing.c
@@ -1,15 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parsing.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/15 02:09:35 by codespace         #+#    #+#             */
-/*   Updated: 2026/06/15 02:22:21 by codespace        ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   parsing.c                                         :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/06/15 02:09:35 by username         #+#    #+#              */
+/*   Updated: 2026/06/15 15:13:01 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 //1.parsing 2 cases: multi argvs -> already separated
 //                   argv with spaces -> ft_split first to kill spaces
@@ -17,17 +16,11 @@
 
 #include "push_swap.h"
 
-int main(int argc, char *argv[])
+int	main(int argc, char **argv)
 {
-
-    if(argc < 2)
-        return(0);
-    return(1);    
-
-    while(**argv != '\0')
-    {
-        if(**argv == ' ')
-            ft_split(argv);
-    }
+	if (argc < 2)
+		return (0);
+	else
+		(**argv == ' ');
+	ft_split(argv);
 }
-

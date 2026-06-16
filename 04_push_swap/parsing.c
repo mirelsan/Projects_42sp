be4fb386f24e87634ecbeffcda086304fa70575c
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 <mirelsan42student@gmail.com>
2026-06-16 15:57:59 -0300

    add new functions

--- 04_push_swap/parsing.c
+++ 04_push_swap/parsing.c
@@ -6,7 +6,7 @@
 /*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/06/15 02:09:35 by username         #+#    #+#              */
-/*   Updated: 2026/06/16 15:14:24 by username        ###   ########.fr        */
+/*   Updated: 2026/06/16 15:56:47 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@
 
 t_stack	*ft_new_stack(void)
 {
-	t_stack	*ptr = malloc(sizeof(t_stack));
+	t_stack	*ptr;
 
+	*ptr = malloc(sizeof(t_stack));
 	if (!ptr)
 		return (NULL);
 	ptr->head = NULL;
@@ -32,6 +33,28 @@ void	ft_add_front(t_stack **stack, t_node *new)
 {
 	new->next = (*stack)->head;
 	(*stack)->head = new;
+}
+
+void	ft_add_tail(t_stack **stack, t_node *new)
+{
+	if ((*stack)->head == NULL)
+		(*stack)->tail = new;
+	new->next = (*stack)->head;
+	(*stack)->head = new;
+	(*stack)->size++;
+}
+
+t_node	*ft_new_node(int data)
+{
+	t_node	*new_node;
+
+	new_node = malloc(sizeof(t_node));
+	if (!new_node)
+		return (NULL);
+	new_node->data = data;
+	new_node->index = 0;
+	new_node->next = NULL;
+	return (new_node);
 }
 
 int	main(int argc, char **argv)

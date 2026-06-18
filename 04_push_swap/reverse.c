53af79b28885420803b11b8d783b28a5c5f83585
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 <mirelsan42student@gmail.com>
2026-06-18 16:09:55 -0300

    add operations files

---
+++ 04_push_swap/reverse.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   reverse.c                                         :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/06/18 15:55:20 by username         #+#    #+#              */
+/*   Updated: 2026/06/18 15:56:11 by username        ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Reverse Rotate(rra, rrb, rrr) : Shift all elements of the stack down by 1 position(the last element becomes the first).
+
+#include "push_swap.h"
---
+++ 04_push_swap/rotate.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   rotate.c                                          :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/06/18 15:57:23 by username         #+#    #+#              */
+/*   Updated: 2026/06/18 15:58:03 by username        ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Rotate(ra, rb, rr) : Shift all elements of the stack up by 1 position(the 1st element becomes the last).
+
+#include "push_swap.h"

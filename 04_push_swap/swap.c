eacc8ca4957689a06220b7a7cbb8601fe7ee722b
adedias- <adedias-@student.42sp.org.br>
2026-06-19 16:00:39 -0300

    finalizado o swap.c

--- 04_push_swap/swap.c
+++ 04_push_swap/swap.c
@@ -6,7 +6,7 @@
 /*   By: adedias- <adedias-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 15:56:24 by username          #+#    #+#             */
-/*   Updated: 2026/06/18 17:10:46 by adedias-         ###   ########.fr       */
+/*   Updated: 2026/06/19 15:00:15 by adedias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,30 @@
 
 void swap(t_stack  *stack)
 {
+	int tmp;
+
+	if (!stack || !stack->head || !stack->head->next)
+		return ;
+	tmp = stack->head->data;
+	stack->head->data = stack->head->next->data;
+	stack->head->next->data = tmp;
+}
+void sa(t_stack  *stack, t_stack *a)
+{
+	swap(a);
+	write(1, "sa\n", 3);
+}
+
+void sb(t_stack  *stack, t_stack  *b)
+{
+	swap(b);
+	write(1, "sb\n", 3);
+}
+void ss(t_stack  *stack, t_stack  *a, t_stack  *b)
+{
 	t_node *tmp;
 
-	tmp->data = stack->head->data;
-	stack->head->data = stack->head->next->data;
-	stack->head->next->data = tmp->data;
+	swap(a);
+	swap(b);
+	write(1, "ss\n", 3);
 }

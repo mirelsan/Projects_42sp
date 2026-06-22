75ff42b45739aae12ebf300cf9387bb3a5aaef37
Mirela Santos Oliveira <mirelsan42student@gmail.com>
2026-06-22 21:18:34 +0000

    correct disorder function

--- 04_push_swap/disorder.c
+++ 04_push_swap/disorder.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                       :::      ::::::::    */
-/*   disorder.c                                        :+:      :+:    :+:    */
-/*                                                   +:+ +:+         +:+      */
-/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
-/*                                               +#+#+#+#+#+   +#+            */
-/*   Created: 2026/06/22 15:14:11 by username         #+#    #+#              */
-/*   Updated: 2026/06/22 16:31:43 by username        ###   ########.fr        */
+/*                                                        :::      ::::::::   */
+/*   disorder.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/22 15:14:11 by username          #+#    #+#             */
+/*   Updated: 2026/06/22 21:16:33 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,29 @@
 
 double	disorder(t_stack *stack)
 {
-	int		mistakes;
-	int		total;
+	double	mistakes;
+	double	total;
 	t_node	*outer;
 	t_node	*inner;
 
 	mistakes = 0;
 	total = 0;
-	t_node	outer = *stack->head;
+	outer = stack->head;
 
 	while (outer != NULL && outer->next != NULL)
 	{
-		t_node	*inner = outer->next;
+		inner = outer->next;
 
 		while (inner != NULL)
 		{
+			total += 1;
+			if (outer->data > inner->data)
+			{
+				mistakes += 1;
+			}
 			inner = inner->next;
 		}
-		outer = outer->next;
-		total += 1;
-		if (outer > inner)
-			mistakes += 1;
-		return (mistakes / total);
+		outer = outer->next;			
 	}
+	return (mistakes / total);
 }

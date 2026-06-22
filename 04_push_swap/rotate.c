068cd217254bf65dd56b200d47d4caaaa2568842
Mirela Santos Oliveira <mirelsan42student@gmail.com>
2026-06-22 00:31:51 +0000

    start updating files

--- 04_push_swap/rotate.c
+++ 04_push_swap/rotate.c
@@ -1,15 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                       :::      ::::::::    */
-/*   rotate.c                                          :+:      :+:    :+:    */
-/*                                                   +:+ +:+         +:+      */
-/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
-/*                                               +#+#+#+#+#+   +#+            */
-/*   Created: 2026/06/18 15:57:23 by username         #+#    #+#              */
-/*   Updated: 2026/06/18 15:58:03 by username        ###   ########.fr        */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/18 15:57:23 by username          #+#    #+#             */
+/*   Updated: 2026/06/22 00:31:43 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //Rotate(ra, rb, rr) : Shift all elements of the stack up by 1 position(the 1st element becomes the last).
 
 #include "push_swap.h"
+
+void rotate(t_stack *stack)
+{
+    t_node old_head;
+}

a74ac4e5c51eef3174b2d59eaeb967e45483f3fb
adedias- <adedias-@student.42sp.org.br>
2026-06-22 10:45:15 -0300

    completado as funções do rotate.c

--- 04_push_swap/rotate.c
+++ 04_push_swap/rotate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: adedias- <adedias-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 15:57:23 by username          #+#    #+#             */
-/*   Updated: 2026/06/22 00:31:43 by codespace        ###   ########.fr       */
+/*   Updated: 2026/06/22 10:44:49 by adedias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,32 @@
 
 void rotate(t_stack *stack)
 {
-    t_node old_head;
+	t_node *first;
+	
+	if (!stack || !stack->head || !stack->head->next)
+		return (NULL);
+	first = stack->head;
+	stack->head = first->next;
+	first->next = NULL;
+	if (stack->tail)
+		stack->tail->next = NULL;
+	stack->tail = first;	
+}
+void ra(t_stack *a)
+{
+	rotate(a);
+	write(1, "ra\n", 3);
+}
+
+void rb(t_stack *b)
+{
+	rotate(b);
+	write(1, "ra\n", 3);
+}
+
+void rr(t_stack *a, t_stack *b)
+{
+	rotate(a);
+	rotate(b);
+	write(1, "rr\n", 3);
 }

66e7d2a73b89290f2abdfb2e0edf749511e7067c
Mirela Santos Oliveira <mirelsan42student@gmail.com>
2026-07-06 23:41:18 +0000

    remove 42 header

--- 05_Python_Modules/Python_Module_00/ex6/ft_count_harvest_recursive.py
+++ 05_Python_Modules/Python_Module_00/ex6/ft_count_harvest_recursive.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_count_harvest_recursive.py                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: codespace <codespace@student.42.fr>        +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/07/06 01:09:12 by codespace         #+#    #+#              #
-#    Updated: 2026/07/06 01:27:39 by codespace        ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_count_harvest_recursive():
     x = int(input("Days until harvest: "))
     helper(1, x)
--- 05_Python_Modules/Python_Module_00/ex7/ft_seed_inventory.py
+++ 05_Python_Modules/Python_Module_00/ex7/ft_seed_inventory.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_seed_inventory.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: codespace <codespace@student.42.fr>        +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/07/06 01:10:25 by codespace         #+#    #+#              #
-#    Updated: 2026/07/06 01:41:43 by codespace        ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     if unit == "packets":
         print(seed_type.capitalize() + " seeds: " + str(quantity) + " packets available")
